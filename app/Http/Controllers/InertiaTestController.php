f124f90cc6db7fe4a77a8453333bd1b80ee24069
kan0803 <kan20020803@gmail.com>
2024-04-22 22:31:14 +0900

    “aaa”

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\InertiaTest;
+use Illuminate\Support\Facades\Log;
 
 class InertiaTestController extends Controller
 {
@@ -30,17 +31,31 @@ class InertiaTestController extends Controller
 
     public function store(Request $request)
     {
-        $request -> validate([
-            'title' => ['required', 'max:20'],
-            'content' => ['required'],
+        Log::debug('before');
+        Log::debug($request);
+
+        // $validate = $request->validate([
+        //     'title' => ['required', 'max:20'],
+        //     'content' => ['required'],
+        // ]);
+
+        $request->validate([
+            'title' => 'required|unique:posts|max:20',
+            'content' => 'required',
         ]);
 
+        Log::debug('message');
+        Log::debug($request);
+        Log::debug($validate);
+
         $inertiaTest = new InertiaTest;
-        $inertiaTest -> title = $request -> title;
-        $inertiaTest -> content = $request -> content;
+        $inertiaTest->title = $request->title;
+        $inertiaTest->content = $request->content;
+        $inertiaTest->save();
 
-        $inertiaTest -> save();
-
-        return to_route('inertia.index');
+        return to_route('inertia.index')
+        ->with([
+            'message' => '登録しました。'
+        ]);
     }
 }
